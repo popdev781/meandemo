2354d0fd9a8eef513efb3ff6965b7d5e7085ca5f
Praveen Chandra Thapliyal <pthapliyal781@gmail.com>
2016-12-17 02:38:31 +0530

    added model for 2 way data binding
    added model for 2 way data binding

--- client/js/controllers/meetupsController.js
+++ client/js/controllers/meetupsController.js
@@ -1,6 +1,16 @@
 var app = angular.module("meetupDemo", []);
 
 app.controller("meetupsController",['$scope', function($scope){
-	$scope.meetupsCount= 10;
+
+	$scope.meetups = [
+		{name:"test data"},
+		{name:"data 2"}
+	];
+
+	$scope.createMeetup = function(){
+		console.log("hehe");
+		$scope.meetups.push({ name: $scope.meetupName });
+		$scope.meetupName ='';
+	};
 }]);
 
